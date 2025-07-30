a4916599ee0eafc8f5029ed23906f470792bea8a
Layendan <layendan.h@gmail.com>
2025-07-29 18:38:04 -0700

    Add migration Dockerfile and update docker-compose configurations

--- migrate_schema.sh
+++
@@ -1,7 +0,0 @@
-npx drizzle-kit generate
-
-docker run --rm \
-  --network=soberstreak_default \
-  -v $(pwd)/migrations:/app/migrations \
-  --env-file $(pwd)/.env.db \
-  node:24.0.2-alpine3.21 npx drizzle-kit migrate
