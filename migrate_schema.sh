8ffc6557c69d575f4fae25b5a6678ce60ad61712
Layendan <layendan.h@gmail.com>
2025-07-29 17:56:39 -0700

    Change migration script to use the same node container as compose

--- migrate_schema.sh
+++ migrate_schema.sh
@@ -4,4 +4,4 @@ docker run --rm \
   --network=soberstreak_default \
   -v $(pwd)/migrations:/app/migrations \
   --env-file $(pwd)/.env.db \
-  node:20 npx drizzle-kit migrate
+  node:24.0.2-alpine3.21 npx drizzle-kit migrate
